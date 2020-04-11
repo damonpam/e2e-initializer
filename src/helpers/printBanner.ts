792ad5dc4257ca0bf398c3900010387773ff31ba
Daniel Montesinos <daniel.montesinos@e2ycommerce.com>
2020-04-11 13:14:31 +0200

    Update tsconfig

--- src/helpers/printBanner.ts
+++ src/helpers/printBanner.ts
@@ -1,10 +1,10 @@
 import clear from 'clear';
 import { Options, textSync } from 'figlet';
-import { summer } from 'gradient-string';
+import { fruit } from 'gradient-string';
 
 export function printBanner(text: string) {
   clear();
 
   const options: Options = { font: 'DOS Rebel', horizontalLayout: 'full', verticalLayout: 'fitted' };
-  console.log(summer(textSync(text, options)));
+  console.log(fruit(textSync(text, options)));
 }
